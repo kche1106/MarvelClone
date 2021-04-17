7352c3a0f6c25151eeb8aa6a7fee4c6d0f2b6873
kche1106 <kche011106@gmail.com>
2021-04-17 15:33:39 +0900

    Midterm

---
+++ Mid-2/js/main.js
@@ -0,0 +1,17 @@
+// timer();
+// var current=0;
+// var $interval;
+
+// function timer(){
+//   var $interval=setInterval(function(){slide()},2000);                        
+// }
+
+// function slide(){
+//   $(".slide").animate({left:"-=187px"},1000,function(){
+//     $(this).css({"left":0});
+//     $(".slide").append( $("ul").children("li").eq(0) );
+//   });    
+//   current++;
+//   if(current==5)current=0;
+// }    
+
